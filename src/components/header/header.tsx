b5d8c855f9909e1356e19811d587d037ca0ea361
Olivier <olivier.winkler@icloud.com>
2023-06-30 20:37:18 +0200

    feat(header): fix activeLink styling

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -21,10 +21,9 @@ const NavItem = ({ href, children }: { href: string; children: ReactNode }) => {
         'relative',
         'px-3',
         'py-2',
-        'text-white/50',
         'transition',
         'transition-color',
-        isActive ? 'text-white' : 'hover:text-white/80',
+        isActive ? 'text-white' : 'text-white/50 hover:text-white/80',
       ].join(' '),
     activeLink: [
       'absolute',
