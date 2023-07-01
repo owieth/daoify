a1d3268b0142279434d6a3193882baa158a7b997
Olivier <olivier.winkler@icloud.com>
2023-07-01 15:47:23 +0200

    feat(page): add dashboard illustration

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import GridVector from '@/components/grid-vector/grid-vector';
 import Section from '@/components/section/section';
 import Teaser from '@/components/teaser/teaser';
+import Image from 'next/image';
 
 const styles = {
   main: ['flex', 'flex-col', 'items-center', 'min-h-screen', 'p-24'].join(' '),
@@ -24,7 +25,9 @@ export default function Home() {
       </Section>
 
       <Section>
-        <p>jsdfölds</p>
+        <div className='relative p-16 lg:p-24'>
+          <Image src={"/dashboard.svg"} alt='' height={800} width={1600} />
+        </div>
       </Section>
     </main>
   );
