378929782060224f5b913220e6792e5231964323
Olivier <olivier.winkler@icloud.com>
2023-07-03 19:48:37 +0200

    feat(components): add features component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import Features from '@/components/features/features';
 import GridVector from '@/components/grid-vector/grid-vector';
 import Section from '@/components/section/section';
 import Teaser from '@/components/teaser/teaser';
@@ -34,6 +35,15 @@ export default function Home() {
       <Section>
         <div className="relative p-16 lg:p-24">
           <Image src={'/dashboard.svg'} alt="" height={800} width={1600} />
+        </div>
+      </Section>
+
+      <Section>
+        <span className="text-center text-base">Features</span>
+        <h2 className="text-md font-bold">Cutting-edge features for DAOs</h2>
+
+        <div className="relative p-16 lg:p-24">
+          <Features />
         </div>
       </Section>
     </main>
---
+++ src/components/features/features.tsx
@@ -0,0 +1,56 @@
+import Logo from '../logo/logo';
+
+const Feature = () => {
+  const styles = {
+    wrapper: ['flex', 'flex-col', 'gap-5', 'items-center'].join(' '),
+    iconWrapper: [
+      'inline-flex',
+      'justify-center',
+      'items-center',
+      'h-16',
+      'w-16',
+      'rounded-full',
+      'bg-gradient-to-tr from-black via-black to-white',
+      'p-[2px]',
+      'backdrop-blur-sm',
+    ].join(' '),
+    icon: [
+      'flex',
+      'justify-center',
+      'items-center',
+      'w-full',
+      'h-full',
+      'rounded-full',
+      'bg-black/80',
+    ].join(' '),
+  };
+
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.iconWrapper}>
+        <div className={styles.icon}>
+          <Logo height={24} width={24} />
+        </div>
+      </div>
+
+      <span className="font-bold">Share team box</span>
+
+      <p>Lorem ipsum dolor sit amet consectetur. Donec mi sit varius et.</p>
+    </div>
+  );
+};
+
+const Features = () => {
+  return (
+    <div className="grid grid-cols-3 grid-rows-2 gap-10">
+      <Feature />
+      <Feature />
+      <Feature />
+      <Feature />
+      <Feature />
+      <Feature />
+    </div>
+  );
+};
+
+export default Features;
