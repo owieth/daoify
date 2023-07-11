707e54e438c3039b820fe23f28e24b10df2b0e58
Olivier <olivier.winkler@icloud.com>
2023-07-11 19:20:57 +0200

    feat(section): add margin param for better spacing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -29,7 +29,7 @@ export default function Home() {
 
       <Teaser />
 
-      <Section>
+      <Section margin={32}>
         <h1 className="text-lg font-bold">Latest Legal Wrapper</h1>
         <span className="w-2/4 text-center text-base">
           Zero code, maximum speed. Make professional sites easy, fast and fun
@@ -37,7 +37,7 @@ export default function Home() {
         </span>
       </Section>
 
-      <Section>
+      <Section margin={32}>
         <div className="relative p-16 lg:p-24">
           <Image src={'/dashboard.svg'} alt="" height={800} width={1600} />
         </div>
--- src/components/section/section.tsx
+++ src/components/section/section.tsx
@@ -1,12 +1,23 @@
 import { ReactNode } from 'react';
 
 type Props = {
+  margin?: number;
   children: ReactNode;
 };
 
-const Section = ({ children }: Props) => {
+const Section = ({ margin, children }: Props) => {
+  const styles = {
+    section: [
+      margin ? `mt-${margin}` : 'mt-[400px]',
+      'flex',
+      'flex-col',
+      'items-center',
+      'w-full'
+    ].join(' ')
+  };
+
   return (
-    <section className="mt-32 flex w-full flex-col items-center">
+    <section className={styles.section}>
       {children}
     </section>
   );
