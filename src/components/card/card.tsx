6040b3871211f4b6bbbe26ff62dd1e19a1006ef5
Olivier <olivier.winkler@icloud.com>
2023-08-01 09:03:42 +0200

    feat(card): add card component

---
+++ src/components/card/card.tsx
@@ -0,0 +1,67 @@
+'use client';
+
+import { ReactNode, useRef, useState } from 'react';
+
+type Props = {
+  shiny?: boolean;
+  children: ReactNode;
+};
+
+export const Card = ({ shiny, children }: Props) => {
+  const divRef = useRef<HTMLDivElement>(null);
+  const [isFocused, setIsFocused] = useState(false);
+  const [position, setPosition] = useState({ x: 0, y: 0 });
+  const [opacity, setOpacity] = useState(0);
+
+  const handleMouseMove = (e: React.MouseEvent<HTMLDivElement>) => {
+    if (!divRef.current || isFocused) return;
+
+    const div = divRef.current;
+    const rect = div.getBoundingClientRect();
+
+    setPosition({ x: e.clientX - rect.left, y: e.clientY - rect.top });
+  };
+
+  const handleFocus = () => {
+    setIsFocused(true);
+    setOpacity(1);
+  };
+
+  const handleBlur = () => {
+    setIsFocused(false);
+    setOpacity(0);
+  };
+
+  const handleMouseEnter = () => setOpacity(1);
+
+  const handleMouseLeave = () => setOpacity(0);
+
+  return (
+    <div
+      ref={divRef}
+      onMouseMove={handleMouseMove}
+      onFocus={handleFocus}
+      onBlur={handleBlur}
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+      className="bg-gradient relative overflow-hidden rounded-3xl bg-gradient-to-tr from-neutral-50 to-neutral-100 p-px dark:from-neutral-950 dark:to-neutral-900"
+    >
+      <div className="user-select-none center pointer-events-none absolute left-1/2 top-0 h-px w-[300px] max-w-[300px] -translate-x-1/2 bg-gradient-to-r from-white/0 from-0% via-white/30 via-50% to-white/0 to-100%"></div>
+      <div className="user-select-none center pointer-events-none absolute -top-1 left-1/2 h-[200px] w-full max-w-[200px] -translate-x-1/2 -translate-y-1/2 rounded-full bg-white/10 blur-3xl md:max-w-[400px]"></div>
+
+      {shiny && (
+        <div
+          className="pointer-events-none absolute -inset-px opacity-0 transition duration-300"
+          style={{
+            opacity,
+            background: `radial-gradient(600px circle at ${position.x}px ${position.y}px, rgba(255,255,255,.06), transparent 40%)`,
+          }}
+        />
+      )}
+
+      <div className="grid gap-16 rounded-[23px] bg-gradient-to-tr from-white to-neutral-50 p-8 dark:from-black dark:to-neutral-950">
+        {children}
+      </div>
+    </div>
+  );
+};
--- src/components/feedbacks/feedbacks.tsx
+++ src/components/feedbacks/feedbacks.tsx
@@ -1,19 +1,23 @@
+import { Card } from '../card/card';
+
 const Feedback = () => (
-  <div className="w-[450px] rounded-xl border border-line bg-black p-8 drop-shadow-lg">
-    <blockquote>
-      &quot;This platform helped me increase my sales and revenue. They&apos;re
-      really awesome&quot;
-    </blockquote>
+  <Card shiny>
+    <div className="w-[450px] drop-shadow-lg">
+      <blockquote>
+        &quot;This platform helped me increase my sales and revenue.
+        They&apos;re really awesome&quot;
+      </blockquote>
 
-    <div className="flex gap-6 pt-8">
-      <img src="favicon.ico" height={50} width={50} />
+      <div className="flex gap-6 pt-8">
+        <img src="favicon.ico" height={50} width={50} />
 
-      <div className="flex flex-col justify-between text-sm">
-        <span>DAOify User</span>
-        <span>ACME inc.</span>
+        <div className="flex flex-col justify-between text-sm">
+          <span>DAOify User</span>
+          <span>ACME inc.</span>
+        </div>
       </div>
     </div>
-  </div>
+  </Card>
 );
 
 const Feedbacks = () => {
