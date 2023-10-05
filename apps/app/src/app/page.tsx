53d05053d6d7e3ef5806245a28482c193125b9ba
Olivier <olivier.winkler@icloud.com>
2023-10-05 19:26:09 +0200

    feat(app): add sidebar component

--- apps/app/src/app/page.tsx
+++ apps/app/src/app/page.tsx
@@ -1,10 +1,22 @@
-import { Button } from '@ui/components/button';
+import { Metadata } from 'next';
+import { Sidebar } from '../components/sidebar';
 
-export default function Page() {
+export const metadata: Metadata = {
+  title: 'Music App',
+  description: 'Example music app using the components.',
+};
+
+export default function MusicPage() {
   return (
-    <>
-      <h1>Web</h1>
-      <Button>Click me</Button>
-    </>
+    <div className="border-t">
+      <div className="bg-background">
+        <div className="grid lg:grid-cols-5">
+          <Sidebar />
+          <div className="col-span-3 lg:col-span-4 lg:border-l">
+            <div className="h-full px-4 py-6 lg:px-8"></div>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
