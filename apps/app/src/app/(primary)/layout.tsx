fe4cf740b3f2e7d8c65fb75d04658a204d7faa11
Olivier <olivier.winkler@icloud.com>
2023-10-16 11:23:14 +0200

    feat(app): restructure pages for shared layout

---
+++ apps/app/src/app/(primary)/layout.tsx
@@ -0,0 +1,15 @@
+import { Sidebar } from '@/src/components/sidebar';
+import { Card } from '@ui/components/ui/card';
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div className="grid h-full gap-4 lg:grid-cols-5">
+      <Sidebar />
+      <Card className="col-span-3 flex flex-col lg:col-span-4">{children}</Card>
+    </div>
+  );
+}
