1dbaaff9e49ad0f9591bf175474841be29eb9051
Olivier <olivier.winkler@icloud.com>
2023-10-16 13:34:52 +0200

    feat(app): wrap link around listitem

--- apps/app/src/components/overview/listitem.tsx
+++ apps/app/src/components/overview/listitem.tsx
@@ -1,14 +1,17 @@
 import { Card, CardContent } from '@ui/components/ui/card';
 import { DaoType } from '../../shared/types/models';
 import DaoAvatar from '../dao-avatar';
+import Link from 'next/link';
 
 export default function ListItem({ item }: { item: DaoType }) {
   return (
-    <Card>
-      <CardContent className="align-center flex p-4">
-        <DaoAvatar src={item.image} alt={item.name} />
-        <span>{item.name}</span>
-      </CardContent>
+    <Card className="hover:bg-slate-100">
+      <Link href={`/${item.name}`}>
+        <CardContent className="flex items-center p-4">
+          <DaoAvatar src={item.image} alt={item.name} />
+          <span>{item.name}</span>
+        </CardContent>
+      </Link>
     </Card>
   );
 }
